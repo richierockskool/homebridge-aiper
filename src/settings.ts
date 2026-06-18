91681b02d4c697e4c571ea667b0e656a00431777
Rich <38402006+richierockskool@users.noreply.github.com>
2026-06-18 14:10:27 -0400

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'AiperPlugin';
+export const PLATFORM_NAME = 'homebridge-aiper';
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
