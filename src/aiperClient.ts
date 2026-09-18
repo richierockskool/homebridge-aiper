def0e6e63494936eb1e1d53496fe41d759dc90f0
richierockskool <38402006+richierockskool@users.noreply.github.com>
2026-09-18 11:45:08 -0400

    delete log

--- src/aiperClient.ts
+++ src/aiperClient.ts
@@ -585,9 +585,7 @@ export class AiperClient {
   }
 
   private async performMqttSessionRefresh(): Promise<void> {
-    this.log.warn(
-      'Aiper MQTT DIAGNOSTIC BUILD 2026-09-16-A',
-    );
+    
     this.log.info(
       'Aiper MQTT session is stale. Refreshing Aiper/AWS connection...',
     );
